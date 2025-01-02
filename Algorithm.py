99d60f6b8ffa97ae1f26dfdf210125b6ef828530
alexyegh16 <ayeghiayan@outlook.com>
2025-01-02 15:36:27 -0500

    fixed brackets

--- Algorithm.py
+++ Algorithm.py
@@ -38,12 +38,12 @@ def genlaby():
 
     #bordure du laby (0 à 19)
     for posx in range(19):
-        laby[posx,19].visite=True
-        laby[posx,0].visite=True
+        laby[posx][19].visite=True
+        laby[posx][0].visite=True
 
     for posy in range(19):
-        laby[0,posy].visite=True
-        laby[19,posy].visite=True
+        laby[0][posy].visite=True
+        laby[19][posy].visite=True
 
 
     compteur=1 #le trajet est pas encore commencé
@@ -55,49 +55,51 @@ def genlaby():
     while compteur>0 and compteur<325:
         hasard=random.randint(1, 4) # on génère un nombre entre 1 et 4 pour déterminer la direction
         #si on est bloqué, retourne à la dernière case avec d'autres possibilités
-        if laby[posx,posy+1].visite==True and laby[posx,posy-1].visite==True and laby[-1,posy].visite==True and laby[posx+1,posy].visite==True:
-            laby[posx,posy].visite=True
+        if laby[posx][posy+1].visite==True and laby[posx][posy-1].visite==True and laby[-1][posy].visite==True and laby[posx+1][posy].visite==True:
+            laby[posx][posy].visite=True
             posx=trajet[compteur-1].x
             posy=trajet[compteur-1].y
             compteur=compteur-1
 
             #bas=1
-        elif hasard==1 and laby[posx,posy+1].visite==False: #si hasard=1 et la case en bas de nous n'est pas encore visité 
-            laby[posx,posy].visite=True # la case qu'on est dedans est maintenant visité
-            laby[posx,posy].bas=True #détruire mur
-            laby[posx,posy+1].haut=True
+        elif hasard==1 and laby[posx][posy+1].visite==False: #si hasard=1 et la case en bas de nous n'est pas encore visité 
+            laby[posx][posy].visite=True # la case qu'on est dedans est maintenant visité
+            laby[posx][posy].bas=True #détruire mur
+            laby[posx][posy+1].haut=True
             trajet[compteur].x=posx
             trajet[compteur].y=posy  #garder en mémoire le trajet de mon algorithme
             compteur=compteur+1  #le nombre de cases visité s'incrémente de 1
             posy=posy+1
 
             #haut=2
-        elif hasard==2 and laby[posx,posy-1].visite==False: #si hasard=2 et la case en haut de nous n'est pas encore visité 
-            laby[posx,posy].visite=True
-            laby[posx,posy].haut=True
-            laby[posx,posy-1].bas=True
+        elif hasard==2 and laby[posx][posy-1].visite==False: #si hasard=2 et la case en haut de nous n'est pas encore visité 
+            laby[posx][posy].visite=True
+            laby[posx][posy].haut=True
+            laby[posx][posy-1].bas=True
             trajet[compteur].x=posx
             trajet[compteur].y=posy
             compteur=compteur+1
             posy=posy-1
 
             #gauche=3
-        elif hasard==3 and laby[posx-1,posy].visite==False: #si hasard=3 et la case à gauche de nous n'est pas encore visité 
-            laby[posx,posy].visite=True
-            laby[posx,posy].gauche=True
-            laby[posx-1,posy].droite=True
+        elif hasard==3 and laby[posx-1][posy].visite==False: #si hasard=3 et la case à gauche de nous n'est pas encore visité 
+            laby[posx][posy].visite=True
+            laby[posx][posy].gauche=True
+            laby[posx-1][posy].droite=True
             trajet[compteur].x=posx
             trajet[compteur].y=posy
             compteur=compteur+1
             posx=posx-1
 
             #droite=4
-        elif hasard==4 and laby[posx+1,posy].visite==False: #si hasard=4 et la case à droite de nous n'est pas encore visité 
-            laby[posx,posy].visite=True
-            laby[posx,posy].droite=True
-            laby[posx+1,posy].gauche=True
+        elif hasard==4 and laby[posx+1][posy].visite==False: #si hasard=4 et la case à droite de nous n'est pas encore visité 
+            laby[posx][posy].visite=True
+            laby[posx][posy].droite=True
+            laby[posx+1][posy].gauche=True
             trajet[compteur].x=posx
             trajet[compteur].y=posy
             compteur=compteur+1
             posx=posx+1
+
+    return laby
 
